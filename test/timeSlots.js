c6ff1d639cd0f56256055bf6e60015238a90bb74
Alessio Coser <alessio.coser@xpeppers.com>
2020-02-21 14:37:54 +0100

    completed timeSlots

--- test/timeSlots.js
+++ test/timeSlots.js
@@ -10,4 +10,31 @@ describe('timeSlots', () => {
 
     deepEqual(timeSlots, ["12:00", "12:02", "12:04", "12:06", "12:08", "12:10"])
   })
+
+  it('starts from slot containing current time', () => {
+    var date = new Date("2020-01-01T12:21:12")
+    var rangeInMinutes = 10
+
+    var timeSlots = from(date, rangeInMinutes)
+
+    deepEqual(timeSlots, ["12:20", "12:22", "12:24", "12:26", "12:28", "12:30"])
+  })
+
+  it('handles change of hours and day', () => {
+    var date = new Date("2020-01-01T23:55:12")
+    var rangeInMinutes = 10
+
+    var timeSlots = from(date, rangeInMinutes)
+
+    deepEqual(timeSlots, ["23:54", "23:56", "23:58", "00:00", "00:02", "00:04"])
+  })
+
+  it('handles change of hours', () => {
+    var date = new Date("2020-01-01T12:55:12")
+    var rangeInMinutes = 10
+
+    var timeSlots = from(date, rangeInMinutes)
+
+    deepEqual(timeSlots, ["12:54", "12:56", "12:58", "13:00", "13:02", "13:04"])
+  })
 })
