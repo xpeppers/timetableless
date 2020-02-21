a21b3f85e722724b303b91574df655013fc7a211
Alessio Coser <alessio.coser@xpeppers.com>
2020-02-21 15:01:15 +0100

    adds trenitalia library

--- handler.js
+++ handler.js
@@ -1,14 +1,27 @@
 'use strict'
 
-const registrations = require('./lib/registrations')
+const registrationRepository = require('./lib/registrations')
 const timeSlots = require('./lib/timeSlots')
+const trenitalia = require('./lib/trenitalia')
+
+const ONE_HOUR = 60
 
 module.exports.hello = async event => {
-  var slots = timeSlots.from(new Date())
-  var all = await registrations.forAll(slots)
+  var slots = timeSlots.from(new Date(), ONE_HOUR)
+  var registrations = await registrationRepository.forAll(slots)
   // group by train number in order to make one request foreach station
+
+  // registrations.forEach( (registration) => {
+  //   var trainInfo = trenitalia.trainInfo(registration.trainNumber)
+  // })
+
+  // per ogni treno
+  // recuperiamo stazione di partenza: http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/4620
+  // chiediamo info treno: http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/<id-stazione-partenza>/<numero-treno>
+  // per ogni stazione registrata aggiornare il ritardo
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ all, event }, null, 2)
+    body: JSON.stringify({ registrations, event }, null, 2)
   }
 }
---
+++ test/trenitalia.js
@@ -0,0 +1,8 @@
+const { deepEqual } = require("assert")
+const { trainInfo } = require("../lib/trenitalia")
+
+describe('trenitalia', () => {
+  it('TODO', () => {
+    deepEqual(trainInfo("S001"), {})
+  })
+})
