d8327c13825eb7770b6cc63a72ba60f7496806a2
Alessio Coser <alessio.coser@xpeppers.com>
2020-02-24 15:16:07 +0100

    retrieve all delays

--- handler.js
+++ handler.js
@@ -10,15 +10,13 @@ module.exports.updateDelays = async event => {
   var slots = timeSlots.from(new Date(), ONE_HOUR)
   var registrations = await registrationRepository.forAll(slots)
 
-  registrations.forEach((registration) => {
-    var delays = await trenitalia.trainDelays(registration.trainNumber)
-    console.log(`Delays for ${registration.trainNumber}: `, delays)
-
+  var delays = registrations.map((registration) => {
+    return await trenitalia.trainDelays(registration.trainNumber)
     // TODO: aggiornare il ritardo
   })
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ registrations, slots, event }, null, 2)
+    body: JSON.stringify({ registrations, slots, delays, event }, null, 2)
   }
 }
