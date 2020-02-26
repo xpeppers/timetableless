441706bf3385d4624aa7911d7206e4f95af71604
Alessio Coser <alessio.coser@xpeppers.com>
2020-02-26 14:21:48 +0100

    extracted forEach async

--- handler.js
+++ handler.js
@@ -3,17 +3,23 @@
 const registrationRepository = require('./lib/registrations')
 const timeSlots = require('./lib/timeSlots')
 const trenitalia = require('./lib/trenitalia')
+const { forEach } = require('./lib/asyncHelper')
 
 const ONE_HOUR = 60
+
+async function updateDelay(registration) {
+  var delays = await trenitalia.trainDelays(registration.trainNumber)
+
+  return delays.filter((delay) => delay.station === registration.departureStation)
+
+  // TODO: aggiornare il ritardo
+}
 
 module.exports.updateDelays = async event => {
   var slots = timeSlots.from(new Date(), ONE_HOUR)
   var registrations = await registrationRepository.forAll(slots)
 
-  var delays = registrations.map((registration) => {
-    return await trenitalia.trainDelays(registration.trainNumber)
-    // TODO: aggiornare il ritardo
-  })
+  var delays = await forEach(registrations, updateDelay)
 
   return {
     statusCode: 200,
