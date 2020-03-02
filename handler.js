2924f91e3b0b178a90e5bf04af8a91a6c3c0dab5
Alessio Coser <alessio.coser@xpeppers.com>
2020-03-02 14:25:18 +0100

    prepared to attach scheduled event

--- handler.js
+++ handler.js
@@ -7,11 +7,6 @@ const { forEach } = require('./lib/asyncHelper')
 
 const ONE_HOUR = 60
 
-const peekLog = (name) => (value) => {
-  console.log(name, registration)
-  return value
-}
-
 function filterBy(station) {
   return (delays) => delays.filter((delay) => delay.station === station)
 }
@@ -21,25 +16,21 @@ function getFirstDelay(delaysData) {
 }
 
 function update(registration) {
-  return (delay) => registrationRepository.updateDelay(registration, delay).catch(console.error).then(() => {})
+  return delay => registrationRepository.updateDelay(registration, delay).catch(console.error).then(() => {})
 }
 
 function updateDelay(registration) {
-  return trenitalia
-    .trainDelays(registration.trainNumber)
+  return trenitalia.trainDelays(registration.trainNumber)
     .then(filterBy(registration.departureStation))
     .then(getFirstDelay)
     .then(update(registration))
-    .then(peekLog('Updated Registration: '))
 }
 
-module.exports.updateDelays = async event => {
+module.exports.updateDelays = async () => {
   var slots = timeSlots.from(new Date(), ONE_HOUR)
+  console.log('Slots to query:', slots)
   var registrations = await registrationRepository.forAll(slots)
+  console.log('Registrations found:', registrations)
   var updatedRegistrations = await forEach(registrations, updateDelay)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ updatedRegistrations, slots, event }, null, 2)
-  }
+  console.log('Registrations updated:', updatedRegistrations)
 }
