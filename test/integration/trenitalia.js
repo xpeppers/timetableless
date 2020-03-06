e3514efb472ab6e22cb65e2e2b3008958d7c3dbc
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-03-06 15:00:36 +0100

    implemented trenitalia adapter

--- test/integration/trenitalia.js
+++ test/integration/trenitalia.js
@@ -10,4 +10,11 @@ describe('Trenitalia', () => {
     deepEqual(delays[0].station, "S00228")
     deepEqual(delays[13].station, "S00462")
   })
+
+  it('train departure time', async () => {
+    var trenitalia = new Trenitalia()
+    var departureTime = await trenitalia.departureTime('4640', 'S00460')
+
+    equal(departureTime.toISOString(), '2020-03-06T06:21:00.000Z')
+  })
 })
