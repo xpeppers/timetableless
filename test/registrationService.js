0e16b1d812c762f041babbef8c1379b737e27143
Alessio Coser <alessio.coser@xpeppers.com>
2020-03-11 14:32:14 +0100

    addPerson to notify to an already existing registration

--- test/registrationService.js
+++ test/registrationService.js
@@ -6,16 +6,31 @@ const { RegistrationRepository } = require("../lib/registrationRepository")
 const { Trenitalia } = require("../lib/trenitalia")
 const stubLog = () => {}
 
-const registrationRepository = new RegistrationRepository()
-const trenitalia = new Trenitalia()
-
 describe('RegistrationService', () => {
   it('creates a new registration', async () => {
+    const registrationRepository = new RegistrationRepository()
+    const trenitalia = new Trenitalia()
     const repository = mock(registrationRepository)
     const trenitaliaService = mock(trenitalia)
 
     trenitaliaService.expects('departureTime').withArgs('4640', 'S00458').returns(Promise.resolve(new Date('2020-01-01T10:01:00Z')))
+    repository.expects('exists').withArgs('4640', '10:00').returns(Promise.resolve(false)).once()
     repository.expects('create').withArgs(registration(['a@b.c'], '4640', 'S00458', '10:00', '10:01:00', 0)).once()
+
+    await new RegistrationService(registrationRepository, trenitalia, stubLog).addRegistration('a@b.c', '4640', 'S00458')
+
+    repository.verify()
+  })
+
+  it('update an already existing registration', async () => {
+    const registrationRepository = new RegistrationRepository()
+    const trenitalia = new Trenitalia()
+    const repository = mock(registrationRepository)
+    const trenitaliaService = mock(trenitalia)
+
+    trenitaliaService.expects('departureTime').withArgs('4640', 'S00458').returns(Promise.resolve(new Date('2020-01-01T10:01:00Z')))
+    repository.expects('exists').withArgs('4640', '10:00').returns(Promise.resolve(true)).once()
+    repository.expects('addPersonToNotify').withArgs('4640', '10:00', 'a@b.c').once()
 
     await new RegistrationService(registrationRepository, trenitalia, stubLog).addRegistration('a@b.c', '4640', 'S00458')
 
