8349137e05d4f25e6140bd29ab4a3a9e80fcbc44
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-24 18:23:22 +0200

    updated loading image

--- client/src/timetableless.js
+++ client/src/timetableless.js
@@ -10,11 +10,38 @@ function hideError() {
   document.querySelector(".error-message").innerHTML = ""
 }
 
+function showSuccess() {
+  document.querySelector(".succeeded").classList.remove("hidden")
+  document.querySelector(".inputs").classList.add("hidden")
+  document.querySelector("#train-number").value = ""
+  document.querySelector("#station").value = ""
+  document.querySelector("#stationId").setAttribute("value", "")
+  document.querySelector("#stationId").setAttribute("name", "")
+}
+
+function addLoading(button, form) {
+  button.classList.remove("bg-purple-700")
+  button.classList.remove("hover:bg-purple-900")
+  button.classList.add("bg-purple-500")
+  button.classList.add("hover:bg-purple-500")
+  button.setAttribute("disabled", "disabled")
+  form.classList.add("loading")
+  form.classList.add("border-purple-500")
+}
+
+function removeLoading(button, form) {
+  button.classList.add("bg-purple-700")
+  button.classList.add("hover:bg-purple-900")
+  button.classList.remove("bg-purple-500")
+  button.classList.remove("hover:bg-purple-500")
+  button.removeAttribute("disabled")
+  form.classList.remove("loading")
+  form.classList.remove("border-purple-500")
+}
+
 document.querySelectorAll("#form input").forEach(function(element) {
   element.addEventListener("focus", hideError)
 })
-
-let stationId = document.querySelector("#stationId")
 
 new autoComplete({
   selector: '#station',
@@ -32,58 +59,41 @@ new autoComplete({
   },
 
   onSelect: function(e, term, item) {
-    stationId.setAttribute("value", item.getAttribute('data-id'))
-    stationId.setAttribute("name", item.getAttribute('data-val'))
+    document.querySelector("#stationId").setAttribute("value", item.getAttribute('data-id'))
+    document.querySelector("#stationId").setAttribute("name", item.getAttribute('data-val'))
   }
 })
 
 document.querySelector("#station").addEventListener("change", function(e) {
   if(e.target.value === "") {
-    stationId.setAttribute("value", "")
-    stationId.setAttribute("name", "")
+    document.querySelector("#stationId").setAttribute("value", "")
+    document.querySelector("#stationId").setAttribute("name", "")
   }
 })
 
 document.querySelector("#station").addEventListener("blur", function(e) {
   console.log(e.target)
-  e.target.value = stationId.getAttribute('name')
-  e.target.setAttribute("data-id", stationId.getAttribute('value'))
-  e.target.setAttribute("data-val", stationId.getAttribute('name'))
+  e.target.value = document.querySelector("#stationId").getAttribute('name')
+  e.target.setAttribute("data-id", document.querySelector("#stationId").getAttribute('value'))
+  e.target.setAttribute("data-val", document.querySelector("#stationId").getAttribute('name'))
 })
 
 document.querySelector("#submit-button").addEventListener('click', function(e) {
-  e.target.classList.remove("bg-purple-700")
-  e.target.classList.remove("hover:bg-purple-900")
-  e.target.classList.add("bg-purple-500")
-  e.target.classList.add("hover:bg-purple-500")
-  e.target.setAttribute("disabled", "disabled")
+  addLoading(e.target, document.querySelector("#form"))
 
   var email = document.querySelector("#email").value.trim()
   var trainNumber = document.querySelector("#train-number").value.trim()
-  var station = stationId.value
+  var station = document.querySelector("#stationId").value
 
   axios.post(`${process.env.BACKEND_HOST}/registration`, {email: email, trainNumber: trainNumber, station: station})
     .then(function(result) {
-      document.querySelector(".succeeded").classList.remove("hidden")
-      document.querySelector(".inputs").classList.add("hidden")
-      document.querySelector("#train-number").value = ""
-      document.querySelector("#station").value = ""
-      document.querySelector("#stationId").setAttribute("value", "")
-      document.querySelector("#stationId").setAttribute("name", "")
+      showSuccess()
 
-      e.target.removeAttribute("disabled", false)
-      e.target.classList.add("bg-purple-700")
-      e.target.classList.add("hover:bg-purple-900")
-      e.target.classList.remove("bg-purple-500")
-      e.target.classList.remove("hover:bg-purple-500")
+      removeLoading(e.target, document.querySelector("#form"))
     })
     .catch(function(error) {
+      removeLoading(e.target, document.querySelector("#form"))
       showError("I valori inseriti non sono validi")
-      e.target.removeAttribute("disabled", false)
-      e.target.classList.add("bg-purple-700")
-      e.target.classList.add("hover:bg-purple-900")
-      e.target.classList.remove("bg-purple-500")
-      e.target.classList.remove("hover:bg-purple-500")
     })
 })
 
